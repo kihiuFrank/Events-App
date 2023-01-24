babd248e447da7fd119ba8a15595c8f148d81491
kihiuFrank <franklinekihiu@gmail.com>
2023-01-24 11:57:20 +0300

    styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,39 @@
 import Link from "next/link";
+import Image from "next/image";
 
-export const Header = () => (
-  <header>
-    <nav>
-      <Link href="/" passHref>
-        <b>Home</b>
-      </Link>
-
-      <Link href="/events" passHref>
-        <b>Events</b>
-      </Link>
-
-      <Link href="/about-us" passHref>
-        <b>About Us</b>
-      </Link>
-    </nav>
-  </header>
-);
+export const Header = () => {
+  return (
+    <header>
+      <div>
+        <div className="topNav">
+          <Image
+            alt="logo"
+            src={"/images/logo_black.png"}
+            width={50}
+            height={50}
+          />
+          <nav>
+            <ul>
+              <li>
+                <Link href="/" passHref>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="/events" passHref>
+                  Events
+                </Link>
+              </li>
+              <li>
+                <Link href="/about-us" passHref>
+                  About us
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <p className="title"> Sed ut perspiciatis unde omnis</p>
+      </div>
+    </header>
+  );
+};
